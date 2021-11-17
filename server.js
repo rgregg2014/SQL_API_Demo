3fc1e0e68a484db22690c26ba98ab3314a98f1b0
Rachel Gregg <rgregg2014@gmail.com>
2021-11-17 09:18:40 -0500

    added DB connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //DEPENDENCIES
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 
@@ -6,10 +7,22 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //DB CONNECTION
+const db = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "library_db",
+});
+
 //MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //ROUTES
+// CREATE
+
+//  READ
+//  UPDATE
+//   DELETE
 //START SERVER
 app.listen(PORT, console.log(`App listening on http://localhost:${PORT}`));
