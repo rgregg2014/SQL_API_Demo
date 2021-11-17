c905435e38ff1208f37d215980b32d1f97037e5b
Rachel Gregg <rgregg2014@gmail.com>
2021-11-17 13:50:56 -0500

    added basic CRUD routes on server.js, needs to be modded out to apiRoutes.js

--- server.js
+++ server.js
@@ -18,11 +18,50 @@ const db = mysql.createConnection({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//ROUTES
-// CREATE
+//ROUTES and app.use(routes)
+//  CREATE
+app.post("/api/books", (req, res) => {
+  db.query(
+    `INSERT INTO books (title, author, genre)
+  VALUES 
+  ("${req.body.title}", "${req.body.author}", "${req.body.genre}");`,
+    (err, books) => {
+      console.log(`${req.method} request to /api/books`);
+      res.json(books);
+    }
+  );
+});
 
 //  READ
+app.get("/api/books", (req, res) => {
+  db.query(`SELECT * FROM books;`, (err, books) => {
+    console.log(`${req.method} request to /api/books`);
+    res.json(books);
+  });
+});
+
 //  UPDATE
-//   DELETE
+app.put("/api/books/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, author, genre } = req.body;
+  db.query(
+    `UPDATE books 
+    SET title = "${title}", author="${author}", genre="${genre}" 
+    WHERE id=${id};`,
+    (err, books) => {
+      console.log(`${req.method} request to /api/books/:id`);
+      res.json(books);
+    }
+  );
+});
+
+//  DELETE
+app.delete("/api/books/:id", (req, res) => {
+  const { id } = req.params;
+  db.query(`DELETE FROM books WHERE id=${id};`, (err, response) => {
+    res.json(response);
+  });
+});
+
 //START SERVER
 app.listen(PORT, console.log(`App listening on http://localhost:${PORT}`));
