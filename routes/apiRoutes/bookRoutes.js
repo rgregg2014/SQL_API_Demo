acff3297d6865efaab9ce63e1ad1e4a7487dc9a9
Rachel Gregg <rgregg2014@gmail.com>
2021-11-18 23:02:28 -0500

    added full CRUD routes with sequelize syntax. all working on Insomnia

--- routes/apiRoutes/bookRoutes.js
+++ routes/apiRoutes/bookRoutes.js
@@ -24,42 +24,48 @@ router.get("/", async (req, res) => {
   }
 });
 
-// // GET /api/books/:id
-// app.get("/:id", (req, res) => {
-//   const { id } = req.params;
-//   db.query(
-//     `SELECT * FROM books
-//     WHERE id=${id};`,
-//     (err, books) => {
-//       console.log(`${req.method} request to /api/books/:id`);
-//       res.json(books);
-//     }
-//   );
-// });
+// GET /api/books/:id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const bookData = await Book.findOne({ where: { id: id } });
+    res.status(200).json(bookData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// //  UPDATE
-// // PUT /api/books/:id
-// app.put("/:id", (req, res) => {
-//   const { id } = req.params;
-//   const { title, author, genre } = req.body;
-//   db.query(
-//     `UPDATE books
-//       SET title = "${title}", author="${author}", genre="${genre}"
-//       WHERE id=${id};`,
-//     (err, books) => {
-//       console.log(`${req.method} request to /api/books/:id`);
-//       res.json(books);
-//     }
-//   );
-// });
+//  UPDATE
+// PUT /api/books/:id
+router.put("/:id", async (req, res) => {
+  try {
+    const bookData = await Book.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!bookData) {
+      res.status(404).json({ message: "Nothing in our library!" });
+    }
+    res.status(200).json(req.body);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// //  DELETE
-// // DELETE /api/books/:id
-// app.delete("/:id", (req, res) => {
-//   const { id } = req.params;
-//   db.query(`DELETE FROM books WHERE id=${id};`, (err, response) => {
-//     res.json(response);
-//   });
-// });
+//  DELETE
+// DELETE /api/books/:id
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const bookData = await Book.destroy({ where: { id: id } });
+    res.json({ message: "Successfully deleted!" });
+    if (!bookData) {
+      res.json({ message: "Nothing in our library!" });
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
